5c3140581a6526ff15ad54b4e9bff57456b6e403
Korneliia <korneliiaPositive@gmail.com>
2024-02-20 14:08:17 +0100

    create a few components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import DefaultPanel from "./defaultPanel/defaultPanel";
+import {Provider} from "react-redux";
+import store from "./data/store";
 
 function App() {
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <DefaultPanel/>
+        },
+    ]);
     return (
         <div className="App">
+            <Provider store={store}>
+                <RouterProvider router={router}/>
+            </Provider>
         </div>
     );
 }
---
+++ src/defaultPanel/defaultPanel.jsx
@@ -0,0 +1,12 @@
+import Header from "./staticComponents/Header/Header";
+import Footer from "./staticComponents/Footer/Footer";
+
+const defaultPanel = () => {
+    return (
+        <>
+            <Header/>
+            <Footer/>
+        </>
+    )
+}
+export default defaultPanel;
