0bebf495a042cd4bdfbba94326271a7ce4a38b34
Korneliia <korneliiaPositive@gmail.com>
2024-02-22 13:55:49 +0100

    add styles

---
+++ src/defaultPanel/staticComponents/Footer/LogoOfWebsite/LogoOfWebsite.jsx
@@ -0,0 +1,12 @@
+import style from "./LogoOfWebsite.module.css";
+import imgLogo from "../../../../assets/images/herb.png";
+
+const LogoOfWebsite = () => {
+    return (
+        <div className={style.container}>
+            <img src={imgLogo} alt="logo"/>
+            <h4>Благодійний фонд Україна</h4>
+        </div>
+    )
+}
+export default LogoOfWebsite;
--- src/defaultPanel/staticComponents/Footer/QrCode/QrCode.jsx
+++ src/defaultPanel/staticComponents/Footer/QrCode/QrCode.jsx
@@ -1,9 +1,19 @@
 import style from "./QrCode.module.css";
+import QRCode from "react-qr-code";
 
 const QrCode = () => {
+    const value = "sss";
     return (
         <div className={style.container}>
-            QrCode
+            <h4>Мобільна версія:</h4>
+            <QRCode
+                size={256}
+                style={{height: "auto", maxWidth: "100%", width: "60%"}}
+                value={value}
+                fgColor={'#e3c631'}
+                bgColor={'rgba(178,181,187,0.03)'}
+                viewBox={`0 0 256 256`}
+            />
         </div>
     )
 }
