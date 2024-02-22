758adc046a8a67951e793d8dca27cc94778bb338
Korneliia <korneliiaPositive@gmail.com>
2024-02-22 14:59:55 +0100

    add styles

--- src/defaultPanel/dynamicSubPages/Map/Map.jsx
+++ src/defaultPanel/dynamicSubPages/Map/Map.jsx
@@ -1,9 +1,21 @@
 import style from "./Map.module.css";
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 
 const Map = () => {
     return (
         <div className={style.container}>
-            Map
+            <h2>Як Нас знайти?</h2>
+            <MapContainer className={style.map} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[51.505, -0.09]}>
+                    <Popup>
+                        A pretty CSS3 popup. <br/> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
         </div>
     )
 }
--- src/defaultPanel/staticComponents/Footer/QrCode/QrCode.jsx
+++ src/defaultPanel/staticComponents/Footer/QrCode/QrCode.jsx
@@ -2,14 +2,14 @@ import style from "./QrCode.module.css";
 import QRCode from "react-qr-code";
 
 const QrCode = () => {
-    const value = "https://korneliia08.github.io/charitableFoundation/";
+
     return (
         <div className={style.container}>
             <h4>Мобільна версія:</h4>
             <QRCode
                 size={256}
                 style={{height: "auto", maxWidth: "100%", width: "60%"}}
-                value={value}
+                value={window.location.href}
                 fgColor={'#e3c631'}
                 bgColor={'rgba(178,181,187,0.03)'}
                 viewBox={`0 0 256 256`}
