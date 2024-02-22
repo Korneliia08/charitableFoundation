1ca0d629712d063eef62b36f1cfce1be88f27323
Korneliia <korneliiaPositive@gmail.com>
2024-02-22 11:57:43 +0100

    add styles

--- src/defaultPanel/staticComponents/Footer/Footer.jsx
+++ src/defaultPanel/staticComponents/Footer/Footer.jsx
@@ -1,9 +1,18 @@
 import style from "./Footer.module.css";
+import Contacts from "./Contacts/Contacts";
+import OpenHours from "./OpenHours/OpenHours";
+import VisitCounter from "./VisitCounter/VisitCounter";
+import QrCode from "./QrCode/QrCode";
 
 const Footer = () => {
     return (
         <div className={style.container}>
-            Footer
+            <div>
+                <Contacts/>
+                <OpenHours/>
+                <VisitCounter/>
+                <QrCode/>
+            </div>
         </div>
     )
 }
