fd260efacdbb7d621280fe44b07a9606ea96b5c9
Korneliia <korneliiaPositive@gmail.com>
2024-02-22 15:33:16 +0100

    add styles

---
+++ src/components/ArrowToTop/ArrowToTop.jsx
@@ -0,0 +1,35 @@
+import style from "./ArrowToTop.module.css";
+import {faArrowUp} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {useEffect, useState} from "react";
+
+const ArrowToTop = () => {
+    const [arrowToTop, setArrowToTop] = useState(false);
+
+    function checkY() {
+        if (window.pageYOffset > 0) {
+            setArrowToTop(true);
+            return;
+        }
+        setArrowToTop(false);
+    }
+
+    function scrollToTop() {
+        window.scrollTo({top: 0, behavior: "smooth"});
+    }
+
+    useEffect(() => {
+        document.addEventListener("scroll", checkY)
+
+        return () => {
+            document.removeEventListener("scroll", checkY)
+        };
+    }, []);
+    return (
+        <div className={style.container} style={{bottom: arrowToTop ? "20px" : "-50px"}} onClick={scrollToTop}>
+            <FontAwesomeIcon icon={faArrowUp} className={style.arrowToTopIcon}/>
+        </div>
+    )
+
+}
+export default ArrowToTop;
