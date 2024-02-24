75f482f96fc6dcd3ffd1ffe2f76b9f54a45666a4
Korneliia <korneliiaPositive@gmail.com>
2024-02-24 13:12:20 +0100

    add styles

---
+++ src/defaultPanel/dynamicSubPages/ChoosedActivity/ChoosedActivity.jsx
@@ -0,0 +1,10 @@
+import style from "./ChoosedActivity.module.css";
+
+const ChoosedActivity = () => {
+    return (
+        <div className={style.container}>
+            ChoosedActivity
+        </div>
+    )
+}
+export default ChoosedActivity;
