b57ea8ff1274477edecc2da7a26ce4ab330ffd8c
Korneliia <korneliiaPositive@gmail.com>
2024-03-02 19:33:30 +0100

    modifying

--- src/components/ErrorComponent/ErrorComponent.jsx
+++ src/components/ErrorComponent/ErrorComponent.jsx
@@ -1,13 +1,21 @@
 import style from "./ErrorComponent.module.css";
+import {useNavigate} from "react-router-dom";
 
 const ErrorComponent = () => {
+    const navigate = useNavigate();
+
+    function displayHome() {
+        navigate("/");
+    }
+
     return (
         <div className={style.container}>
             <div className={style.containerForBackGroundAndNumber}>
                 <span className={style.spanErrorNumber}>500</span>
                 <div className={style.containerForBackGround}></div>
             </div>
-            <span className={style.mainSpan}>Вибачте за тимчасові незручності. Сталася внутрішня помилка сервера. Наша команда вже працює над вирішенням цього питання. Якщо Вам потрібна негайна допомога, будь ласка, зверніться до нашого адміністратора за номером [ваш контактний номер]. Дякуємо за ваше розуміння та терпіння.</span>
+            <span className={style.mainSpan}>Вибачте за тимчасові незручності. Сталася внутрішня помилка сервера. Наша команда вже працює над вирішенням цього питання. Дякуємо за ваше розуміння та терпіння.</span>
+            <button onClick={displayHome}>Спробуйте ще раз</button>
         </div>
     )
 }
