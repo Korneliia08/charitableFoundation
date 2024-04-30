40ef74df726223821b57297cddf31e1d1ef6db9e
Korneliia <korneliiaPositive@gmail.com>
2024-04-30 23:41:15 +0200

    work

--- src/defaultPanel/staticComponents/Header/BeltForContact/BeltForContact.jsx
+++ src/defaultPanel/staticComponents/Header/BeltForContact/BeltForContact.jsx
@@ -2,10 +2,11 @@ import style from "./BeltForContact.module.css";
 import {faPhone} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import ukrainianHeart from "../../../../assets/images/heartUkrainian.png";
+import {useTranslation} from 'react-i18next';
 
 const BeltForContact = (props) => {
     const data = props.data;
-
+    const {t} = useTranslation('translation', {lng: 'ua'});
     return (
         <div className={style.container}>
             <div className={style.blockForContact}>
@@ -15,7 +16,7 @@ const BeltForContact = (props) => {
             </div>
             <div className={style.blockForHeart}>
                 <img src={ukrainianHeart} role="presentation" alt="" className={style.ukrainianHeartImage}/>
-                <span>Все Буде Україна!</span>
+                <span>{t('nav.belt.rightTitle')}</span>
             </div>
         </div>
     )
