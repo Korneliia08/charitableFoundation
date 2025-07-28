9be79b76f338a8cae1726ab3f038df199e0ada15
Korneliia Mushak <korneliiapositive@gmail.com>
2025-07-28 09:58:57 +0200

    modifying

--- src/defaultPanel/dynamicSubPages/DepartmentCommunications/DepartmentCommunications.jsx
+++ src/defaultPanel/dynamicSubPages/DepartmentCommunications/DepartmentCommunications.jsx
@@ -1,9 +1,10 @@
-import style from "./DepartmentCommunications.module.css";
+import style from "./DepartmentCommunications.module.scss";
 import ArrowBack from "../../../components/ArrowBack/ArrowBack";
 import {useNavigate} from "react-router-dom";
 import ScrollToTop from "../../../components/scrollToTop";
 import {useSelector} from "react-redux";
 import {useTranslation} from "react-i18next";
+import imageRight from "./../../../assets/images/department.jpg";
 
 const DepartmentCommunications = () => {
     const navigate = useNavigate();
@@ -21,15 +22,50 @@ const DepartmentCommunications = () => {
         <div className="containerForSubPage">
             <ScrollToTop/>
             <div className={style.mainContainer}>
-                <div className={style.blockForTitleAndOther}>
-                    <h4>testsetset</h4>
-                    <img src={import.meta.env.VITE_APP_LINKTOAPI + "files/" + data.imageId} role="presentation"
-                         alt=""/>
+                <div className={style.top}>
+                    <div className={style.left}>
+                        <h5 className={style.title}>Відділ комунікацій та медіа Фонду</h5>
+                        <p className={style.description}>
+                            Розповсюдження інформації в Україні і в світі про діяльність Фонду, потреби українських
+                            ветеранів та постраждалих від російської агресії в Україні. Залучення журналістів, блогерів,
+                            волонтерів до поширення правдивої інформації про російсько-українську війну, її причини, хід
+                            та
+                            наслідки.
+                        </p>
+                    </div>
+                    <div className={style.right}>
+                        <img src={imageRight} alt="imageRight" className={style.imageStyle}/>
+                    </div>
                 </div>
-                <div className={style.blockForOnlineDonat}>
-
-                    sets
-                </div>
+                {/*<div className={style.blockForPersons}>*/}
+                {/*    <h4 className={style.title}>Наші співробітники</h4>*/}
+                {/*    <div className={style.persons}>*/}
+                {/*        <div className={style.personBlock}>*/}
+                {/*            <img src={imageRight} alt="person" className={style.imgPerson}/>*/}
+                {/*            <div className={style.blockLinia}>*/}
+                {/*                <div className={style.circle}></div>*/}
+                {/*            </div>*/}
+                {/*            <h5 className={style.name}>Ігор Іванович</h5>*/}
+                {/*            <p className={style.description}>Співзасновник благодійного фонду ітд ітп</p>*/}
+                {/*        </div>*/}
+                {/*        <div className={style.personBlock}>*/}
+                {/*            <img src={imageRight} alt="person" className={style.imgPerson}/>*/}
+                {/*            <div className={style.blockLinia}>*/}
+                {/*                <div className={style.circle}></div>*/}
+                {/*            </div>*/}
+                {/*            <h5 className={style.name}>Ігор Іванович</h5>*/}
+                {/*            <p className={style.description}>Співзасновник благодійного фонду ітд ітп</p>*/}
+                {/*        </div>*/}
+                {/*        <div className={style.personBlock}>*/}
+                {/*            <img src={imageRight} alt="person" className={style.imgPerson}/>*/}
+                {/*            <div className={style.blockLinia}>*/}
+                {/*                <div className={style.circle}></div>*/}
+                {/*            </div>*/}
+                {/*            <h5 className={style.name}>Ігор Іванович</h5>*/}
+                {/*            <p className={style.description}>Співзасновник благодійного фонду ітд ітп</p>*/}
+                {/*        </div>*/}
+                {/*    </div>*/}
+                {/*</div>*/}
             </div>
             <ArrowBack onClickEvent={displayOneStepAgo} className={style.arrow}/>
         </div>
